0900e3457af84084be48b5c9b5e77808be878c04
fankaiqiang <fankaiqiang@okay.cn>
2020-05-22 16:43:45 +0800

    增加参数校验

--- src/main/groovy/com/okay/family/constants/bean/TestCaseBean.groovy
+++ src/main/groovy/com/okay/family/constants/bean/TestCaseBean.groovy
@@ -2,11 +2,16 @@ package com.okay.family.constants.bean
 
 import com.alibaba.fastjson.JSONObject
 import com.okay.family.fun.base.bean.AbstractBean
+import org.hibernate.validator.constraints.Range
+
+import javax.validation.constraints.Min
 
 class TestCaseBean extends AbstractBean {
 
+    @Min(value = 1L, message = "uid错误")
     int uid
 
+    @Range(min = 1L, max = 5L, message = "环境设置错误")
     int environment
 
     int project
@@ -16,7 +21,16 @@ class TestCaseBean extends AbstractBean {
     String host
 
     String apiName
+    /**
+     * 接口类型,1:读;2:写
+     */
+    @Range(min = 1L, max = 2L)
+    int apiType
 
+    /**
+     * 1:读接口,2:写接口
+     */
+    @Range(min = 1L, max = 2L)
     JSONObject method
 
     JSONObject headers
