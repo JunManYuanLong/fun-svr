b9d36944f272a97315131bd14bbde2e26adc0d3e
fankaiqiang <fankaiqiang@okay.cn>
2020-06-08 09:46:52 +0800

    增加公共数据bean

--- src/main/groovy/com/okay/family/common/bean/pubdata/PubDataBean.groovy
+++ src/main/groovy/com/okay/family/common/bean/pubdata/PubDataBean.groovy
@@ -1,8 +1,20 @@
 package com.okay.family.common.bean.pubdata
 
+import com.alibaba.fastjson.JSONObject
 import com.okay.family.fun.base.bean.AbstractBean
 
-class PubDataBean extends AbstractBean{
+class PubDataBean extends AbstractBean {
+
     private static final long serialVersionUID = 8754205931721071606L;
+
+    int id
+
+    int owner
+
+    JSONObject data
+
+    String name
+
+    String mark
 
 }
