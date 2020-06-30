b571cc62827bb4992df8914834b8f87cc3ce6949
fankaiqiang <fankaiqiang@okay.cn>
2020-06-30 11:03:32 +0800

    增加用户状态静态数据接口

---
+++ app.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+version="1.0.1";
+
+appName="family-qa.jar"
+#if [ -z $appName ];then
+#    appName=`ls -t |grep .jar$ |head -n1`
+#fi
+
+function start()
+{
+	count=`ps -ef |grep java|grep $appName|wc -l`
+	if [ $count != 0 ];then
+		echo "Maybe $appName is running, please check it..."
+	else
+		echo "The $appName is starting..."
+		nohup java -jar ./$appName -XX:+UseG1GC -XX:+HeapDumpOnOutOfMemoryError -Xms2G -Xmx2G > /dev/null 2>&1 &
+	fi
+}
+
+function stop()
+{
+	appId=`ps -ef |grep java|grep $appName|awk '{print $2}'`
+	if [ -z $appId ];then
+	    echo "Maybe $appName not running, please check it..."
+	else
+        echo "The $appName is stopping..."
+        kill $appId
+	fi
+}
+
+function restart()
+{
+    # get release version
+    releaseApp=`ls -t |grep .jar$ |head -n1`
+
+    # get last version
+    lastVersionApp=`ls -t |grep .jar$ |head -n2 |tail -n1`
+
+    appName="family-qa.jar"
+    stop
+    for i in {5..1}
+    do
+        echo -n "$i "
+        sleep 1
+    done
+    echo 0
+
+    backup
+
+#    appName=$releaseApp
+    start
+}
+
+#function backup()
+#{
+#    # get backup version
+#    backupApp=`ls |grep -wv $releaseApp$ |grep .jar$`
+#
+#    # create backup dir
+#    if [ ! -d "backup" ];then
+#        mkdir backup
+#    fi
+#
+#    # backup
+#    for i in ${backupApp[@]}
+#    do
+#        echo "backup" $i
+#        mv $i backup
+#    done
+#}
+
+function status()
+{
+    appId=`ps -ef |grep java|grep $appName|awk '{print $2}'`
+	if [ -z $appId ]
+	then
+	    echo -e "\033[31m Not running \033[0m"
+	else
+	    echo -e "\033[32m Running [$appId] \033[0m"
+	fi
+}
+
+
+function usage()
+{
+    echo "Usage: $0 {start|stop|restart|status|stop -f}"
+    echo "Example: $0 start"
+    exit 1
+}
+
+case $1 in
+	start)
+	start;;
+
+	stop)
+	stop;;
+
+	restart)
+	restart;;
+
+	status)
+	status;;
+
+	*)
+	usage;;
+esac
--- src/main/groovy/com/okay/family/controller/CommonDataController.groovy
+++ src/main/groovy/com/okay/family/controller/CommonDataController.groovy
@@ -3,7 +3,9 @@ package com.okay.family.controller;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.okay.family.common.basedata.OkayConstant;
+import com.okay.family.common.enums.CollectionStatus;
 import com.okay.family.common.enums.Identity;
+import com.okay.family.common.enums.RunResult;
 import com.okay.family.common.enums.UserState;
 import com.okay.family.fun.base.bean.Result;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,25 +18,58 @@ import static com.okay.family.fun.frame.SourceCode.getJson;
 @RequestMapping(value = "/common")
 public class CommonDataController {
 
-
-
+/**
+ * 用户身份
+ * @return
+ */
     @GetMapping(value = "/roles")
     public Result getIdentity() {
         JSONArray all = Identity.getAll();
-        return Result.success(all);
+        Result.success(all);
     }
 
 
+/**
+ * 填写规则
+ * @return
+ */
     @GetMapping(value = "/rule")
     public Result getRule() {
         JSONObject json = getJson("text=" + OkayConstant.RULE);
-        return Result.success(json);
+        Result.success(json);
     }
 
+/**
+ * 用户状态
+ * @return
+ */
     @GetMapping(value = "/userstate")
     public Result getUserState() {
-        JSONArray all = UserState.getAll();
-        return Result.success(all);
+        JSONArray all = UserState.getAll()
+        Result.success(all)
     }
+
+
+/**
+ * 用例集状态
+ * @return
+ */
+    @GetMapping(value = "/collectstate")
+    public Result getCollectState() {
+        JSONArray all = CollectionStatus.getAll()
+        Result.success(all)
+    }
+
+
+/**
+ * 用例运行状态
+ * @return
+ */
+    @GetMapping(value = "/runresult")
+    public Result getRunResult() {
+        JSONArray all = RunResult.getAll()
+        Result.success(all)
+    }
+
 
 }
