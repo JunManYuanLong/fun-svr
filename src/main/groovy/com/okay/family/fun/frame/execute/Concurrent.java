689871fb20dd300923e04a9f97836095de80e08f
fankaiqiang <fankaiqiang@okay.cn>
2020-09-17 19:14:06 +0800

    调整压测框架结构,为增加固定QPS压测模式准备

--- src/main/groovy/com/okay/family/fun/frame/execute/Concurrent.java
+++ src/main/groovy/com/okay/family/fun/frame/execute/Concurrent.java
@@ -8,15 +8,16 @@ import com.okay.family.fun.frame.Save;
 import com.okay.family.fun.frame.SourceCode;
 import com.okay.family.fun.utils.Time;
 import com.okay.family.fun.utils.WriteRead;
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -40,7 +41,7 @@ public class Concurrent extends SourceCode {
     /**
      * 任务描述
      */
-    public String desc = "FunTester";
+    public String desc = DEFAULT_STRING;
 
     /**
      * 任务集
@@ -125,7 +126,7 @@ public class Concurrent extends SourceCode {
 
     private Concurrent(int threadNum) {
         this.threadNum = threadNum;
-        executorService = Executors.newFixedThreadPool(threadNum);
+        executorService = ThreadPoolUtil.createFixedPool(threadNum);
         countDownLatch = new CountDownLatch(threadNum);
     }
 
@@ -135,11 +136,13 @@ public class Concurrent extends SourceCode {
 
     /**
      * 执行多线程任务
+     * 默认取list中thread对象,丢入线程池,完成多线程执行,如果没有threadname,name默认采用desc+线程数作为threadname,去除末尾的日期
      */
     public PerformanceResultBean start() {
         startTime = Time.getTimeStamp();
         for (int i = 0; i < threadNum; i++) {
             ThreadBase thread = getThread(i);
+            if (StringUtils.isBlank(thread.threadName)) thread.threadName = desc.replaceAll("\\d{14}$", EMPTY) + i;
             thread.setCountDownLatch(countDownLatch);
             executorService.execute(thread);
         }
@@ -193,39 +196,9 @@ public class Concurrent extends SourceCode {
         List<Integer> data = strings.stream().map(x -> changeStringToInt(x)).collect(toList());
         int sum = data.stream().mapToInt(x -> x).sum();
         Collections.sort(data);
-        String statistics = statistics(data, desc);
+        String statistics = StatisticsUtil.statistics(data, desc, this.threadNum);
         double qps = 1000.0 * size * name / sum;
         return new PerformanceResultBean(desc, start, end, name, size, sum / size, qps, getPercent(executeTotal, errorTotal), getPercent(threadNum, failTotal), executeTotal, statistics);
-    }
-
-    /**
-     * 将性能测试数据图表展示
-     *
-     * <p>
-     * 将数据排序,然后按照循序分桶,选择桶中中位数作代码,通过二维数组转化成柱状图
-     * </p>
-     *
-     * @param data 性能测试数据,也可以其他统计数据
-     * @return
-     */
-    public static String statistics(List<Integer> data, String title) {
-        int size = data.size();
-        if (size < 1000) return EMPTY;
-        int[] ints = range(1, BUCKET_SIZE + 1).map(x -> data.get(size * x / BUCKET_SIZE - size / BUCKET_SIZE / 2)).toArray();
-        int largest = ints[BUCKET_SIZE - 1];
-        String[][] map = Arrays.asList(ArrayUtils.toObject(ints)).stream().map(x -> getPercent(x, largest, BUCKET_SIZE)).collect(toList()).toArray(new String[BUCKET_SIZE][BUCKET_SIZE]);
-        String[][] result = new String[BUCKET_SIZE][BUCKET_SIZE];
-        /*将二维数组反转成竖排*/
-        for (int i = 0; i < BUCKET_SIZE; i++) {
-            for (int j = 0; j < BUCKET_SIZE; j++) {
-                result[i][j] = getManyString(map[j][BUCKET_SIZE - 1 - i], 2) + SPACE_1;
-            }
-        }
-        StringBuffer table = new StringBuffer(LINE + getManyString(TAB, 4) + ((title == null || title.length() == 0) ? DEFAULT_STRING : title) + LINE + LINE + TAB + ">>响应时间分布图,横轴排序分成桶的序号,纵轴每个桶的中位数<<" + LINE + TAB + TAB + "--<中位数数据最小值为:" + ints[0] + " ms,最大值:" + ints[BUCKET_SIZE - 1] + " ms>--" + LINE);
-        for (int i = 0; i < BUCKET_SIZE; i++) {
-            table.append(Arrays.asList(result[i]).stream().collect(Collectors.joining()) + LINE);
-        }
-        return table.toString();
     }
 
 
@@ -248,22 +221,6 @@ public class Concurrent extends SourceCode {
      */
     public PerformanceResultBean countQPS(int name) {
         return countQPS(name, EMPTY, Time.getDate(), Time.getDate());
-    }
-
-    /**
-     * 将数据转化成string数组
-     *
-     * @param part   数据
-     * @param total  基准数据,默认最大的中位数
-     * @param length
-     * @return
-     */
-    public static String[] getPercent(int part, int total, int length) {
-        int i = part * 8 * length / total;
-        int prefix = i / 8;
-        int suffix = i % 8;
-        String s = getManyString(PERCENT[8], prefix) + (prefix == length ? EMPTY : PERCENT[suffix] + getManyString(SPACE_1, length - prefix - 1));
-        return s.split(EMPTY);
     }
 
 
